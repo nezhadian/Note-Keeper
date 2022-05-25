cbf1c28797a2ec5529c84a0a1e5a326377e7f8e5
Your Name <you@example.com>
2022-05-25 17:13:59 +0430

    Button Live Hover And Press Color

---
+++ Classes/ColorSaturateConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+
+namespace Note_Keeper
+{
+    class ShadeSolidColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sourceColor = (SolidColorBrush)parameter;
+
+            return new SolidColorBrush(AddToColor(sourceColor.Color, 20));
+            
+
+        }
+
+        public static Color AddToColor(Color color,int per)
+        {
+            color.R = (byte)Math.Max(Math.Min(color.R + per, 255), 0);
+            color.G = (byte)Math.Max(Math.Min(color.G + per, 255), 0);
+            color.B = (byte)Math.Max(Math.Min(color.B + per, 255), 0);
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class TintSolidColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sourceColor = (SolidColorBrush)parameter;
+
+            return new SolidColorBrush(ShadeSolidColorConverter.AddToColor(sourceColor.Color, -20));
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
